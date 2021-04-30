0421cd592b7ee5edff6f19829e66d55c40db1a52
Yanan Lyu <yanan.lyu@gmail.com>
2021-04-30 19:24:06 -0400

    add bolt db

---
+++ src/coin/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"core"
+)
+
+func main() {
+	bc := core.NewBlockchain()
+
+	defer bc.Db.Close()
+
+	cli := core.CLI{bc}
+	cli.Run()
+}
--- src/core/block.go
+++ src/core/block.go
@@ -3,35 +3,73 @@ package core
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
+	"log"
 	"strconv"
 	"time"
 )
 
-//Block keeps block headers
+// Block represents a block in the blockchain
 type Block struct {
 	Timestamp     int64
-	Data          []byte
+	Transactions  []*Transaction
 	PrevBlockHash []byte
 	Hash          []byte
+	Nonce         int
+	Height        int
 }
 
-// NewBlock creating and return Block
-func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{
-		time.Now().Unix(),
-		[]byte(data),
-		prevBlockHash,
-		[]byte{},
-	}
+// NewBlock creates and returns Block
+func NewBlock(transactions []*Transaction, prevBlockHash []byte, height int) *Block {
+	block := &Block{time.Now().Unix(), transactions, prevBlockHash, []byte{}, 0, height}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
 
-	block.SetHash()
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
-// SetHash calculates and sets block hash
-func (b *Block) SetHash()  {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-	b.Hash = hash[:]
+// NewGenesisBlock creates and returns genesis Block
+func NewGenesisBlock(coinbase *Transaction) *Block {
+	return NewBlock([]*Transaction{coinbase}, []byte{}, 0)
+}
+
+// HashTransactions returns a hash of the transactions in the block
+func (b *Block) HashTransactions() []byte {
+	var transactions [][]byte
+
+	for _, tx := range b.Transactions {
+		transactions = append(transactions, tx.Serialize())
+	}
+	mTree := NewMerkleTree(transactions)
+
+	return mTree.RootNode.Data
+}
+
+// Serialize serializes the block
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+// DeserializeBlock deserializes a block
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
